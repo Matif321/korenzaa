53c56b914d12bf6a53829a48bf889ab29087bae3
Matif321 <matifkhan6038@gmail.com>
2026-02-19 13:44:39 +0500

    i complete product with testing

--- routers/userRoutes.js
+++ routers/userRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { loginUser, reverify, signupUser, verify } from "../controller/UserController.js";
 import { createCategory, deleteCategory, getAllCategories, updateCategory } from "../controller/categoryController.js";
+import { createProduct, deleteProduct, getAllProducts, getSingleProduct, updateProduct } from "../controller/productController.js";
 
 const router = express.Router();
 
@@ -16,5 +17,13 @@ router.post("/createCategory", createCategory);
 router.get("/getAllCategories", getAllCategories);
 router.put("/updateCategory/:id", updateCategory); // id param add
 router.delete("/deleteCategory/:id", deleteCategory); // id param add
+
+// product routes
+
+router.post("/createProduct", createProduct)
+router.get("/ getAllProducts", getAllProducts)
+router.get("/getSingleProduct", getSingleProduct)
+router.put("/updateProduct", updateProduct)
+router.delete("/deleteProduct", deleteProduct)
 
 export default router
